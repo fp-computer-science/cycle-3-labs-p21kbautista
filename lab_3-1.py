17bba47a7fc2b6147c2c070d31785e02ed28b396
Kyle Bautista <p21kbautista@fairfieldprep.org>
2020-09-28 13:12:43 -0400

    Add lab_3-1

---
+++ lab_3-1.py
@@ -0,0 +1,21 @@
+# Question 1
+print("x += 5")
+print("x = x + 5")
+# Question 2
+print("y = y - 3")
+print("y -= 3")
+# Question 3
+print("z **= 2")
+print("z = z ** 2")
+# Question 4
+print("w = w % 2")
+print("w %=2")
+# Question 5
+print("t = t / 5")
+print("t /= 5")
+# Question 6
+print("a *=12")
+print("a = a * 12")
+# Question 7
+print("i = i // 7")
+print("i //= 7")
